b192c5e5d2ada3ddb337d54e21fd1f677d69652c
Ratheesh Ramachandran <rramachandran@healthgrades.com>
2018-09-21 13:15:29 +0530

    polly reference added

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,52 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web.Script.Serialization;
 
 namespace ClientApp
 {
     class Program
     {
+
+        private static HttpClient _client = new HttpClient() {
+            Timeout = TimeSpan.FromSeconds(2)
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter '1 <delay in milli second>' for Delayed products \r\n Enter '2' for Get Products immediately \r\n Enter '3' for Getting Exception \r\n");
-            var s = Console.ReadLine();
-            string[] input = s.Split(' ');
-            switch (input[0])
+            Console.WriteLine("API Timeout set to 2 seconds \r\n------------------\r\n> Enter '1 <number of calls> <delay in milli second>' for Delayed products \r\n> Enter '2 <number of calls>' for Get Products immediately \r\n> Enter '3 <number of calls>' for Getting Exception \r\n");
+            while (true)
             {
-                case "1":
-                    ServiceRequest("http://localhost:52892/api/products/Getdelayedproducts?delay=" + input[1]);
-                    break;
-                case "2":
-                    ServiceRequest("http://localhost:52892/api/products/GetProducts");
-                    break;
-                case "3":
-                    ServiceRequest("http://localhost:52892/api/products/GetException");
-                    break;
-                default:
-                    break;
+                var s = Console.ReadLine();
+                string[] input = s.Split(' ');
+                var numberofCalls = input.Length > 1 ? Convert.ToInt32(input[1]) : 1;
+                switch (input[0])
+                {
+                    case "1":
+                        ServiceRequest($"http://localhost:52892/api/products/Getdelayedproducts?delay={(input.Length > 2 ? input[2] : "1000")}", numberofCalls);
+                        break;
+                    case "2":
+                        ServiceRequest("http://localhost:52892/api/products/GetProducts", numberofCalls);
+                        break;
+                    case "3":
+                        ServiceRequest("http://localhost:52892/api/products/GetException", numberofCalls);
+                        break;
+                    default:
+                        break;
+                }
             }
-
         }
 
-        static async void ServiceRequest(string url)
+        static void ServiceRequest(string url, int number_of_calls = 1)
         {
-            string result = await Get(url);
-            Console.WriteLine(result);
-            Console.Read();
+            try
+            {
+                Console.WriteLine("'" + url + "' api is calling " + number_of_calls + " times");
+                for (int i = 0; i < Convert.ToInt32(number_of_calls); i++)
+                {
+                    Console.WriteLine("\r\n" + GetAsync(url).Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
-        static async Task<string> Get(string url)
+        static async Task<string> GetAsync(string url)
         {
-            using (var client = new HttpClient().GetAsync(new Uri(url)))
+            string result = "";
+            try
             {
-                string content = await client.Result.Content.ReadAsStringAsync();
-                //var yourOjbect = new JavaScriptSerializer().Deserialize<string[]>(content);
-                return content;
+               
+                HttpResponseMessage response = await _client.GetAsync(new Uri(url));
+                if (response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
             }
+            catch (Exception ex)
+            {
+                result = ex.ToString();
+            }
+            return result;
         }
     }
 }
