1b175663fd23c576d7c194b209779962a500dcc8
Ratheesh Ramachandran <rramachandran@healthgrades.com>
2018-09-22 14:22:04 +0530

    added client WepAPI without polly code

---
+++ Client_API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Client_API.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Client_API/Controllers/ClientController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Client_API.Controllers
+{
+    public class ClientController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get()
+        {
+            HttpResponseMessage response = null;
+            using (var client = new HttpClient() { Timeout = TimeSpan.FromSeconds(5)})
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                response = await client.GetAsync(new Uri("http://localhost:52892/api/products/Getproducts"));
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new HttpResponseMessage(response.StatusCode);
+                }
+            }
+            return response;
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetwithDelay(int delay = 0)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                using (var client = new HttpClient() { Timeout = TimeSpan.FromSeconds(5) })
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    response = await client.GetAsync(new Uri($"http://localhost:52892/api/products/GetdelayedProducts?delay={delay}"));
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return new HttpResponseMessage(response.StatusCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return response;
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetException()
+        {
+            HttpResponseMessage response = null;
+            using (var client = new HttpClient() { Timeout = TimeSpan.FromSeconds(5) })
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                response = await client.GetAsync(new Uri($"http://localhost:52892/api/products/GetException"));
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new HttpResponseMessage(response.StatusCode);
+                }
+            }
+            return response;
+        }
+    }
+}
