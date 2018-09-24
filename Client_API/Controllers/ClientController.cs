862f5a3095f08af1d0b7832a390c93c18b45638d
Ratheesh Ramachandran <rramachandran@healthgrades.com>
2018-09-24 17:12:44 +0530

    added poly implmentation in two end points

--- Client_API/Controllers/ClientController.cs
+++ Client_API/Controllers/ClientController.cs
@@ -1,6 +1,7 @@
 ﻿using Polly;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,32 +13,71 @@ namespace Client_API.Controllers
 {
     public class ClientController : ApiController
     {
-        [HttpGet]
-        public async Task<HttpResponseMessage> Get()
+        protected ClientController()
         {
-            return await ServiceCall(new Uri($"http://localhost:52892/api/products/GetProducts"));
+            //if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "My Log File.txt"))
+            //    File.Delete(AppDomain.CurrentDomain.BaseDirectory + "My Log File.txt");
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            LogMessageToFile("-------Calling Get API");
+            Action<Exception, TimeSpan> onBreak = (exception, timespan) =>
+            {
+                LogMessageToFile("On Break, time span : " + timespan.ToString());
+            };
+            Action onReset = () =>
+            {
+                LogMessageToFile("On Reset");
+            };
+            Action onHalfOpen = () =>
+            {
+                LogMessageToFile("on HalfOpen");
+            };
+            var circuitBreaker = Policy
+                                .Handle<Exception>()
+                                .CircuitBreakerAsync(
+                                    exceptionsAllowedBeforeBreaking: 1,
+                                    durationOfBreak: TimeSpan.FromSeconds(20000),
+                                    onBreak: onBreak,
+                                    onReset: onReset,
+                                    onHalfOpen: onHalfOpen
+                                );
+
+            HttpResponseMessage res = null;
+            await circuitBreaker.ExecuteAsync(async () => res = await ServiceCall(new Uri($"http://localhost:52892/api/products/GetProducts")));
+            return await res.Content.ReadAsStringAsync();
         }
 
         [HttpGet]
         public async Task<string> GetwithDelay(int delay = 0)
         {
-            Action<Exception, TimeSpan> onBreak = (exception, timespan) => {
-                Console.WriteLine("On Break");
+            LogMessageToFile("-------Calling GetwithDelay API with dalay " + delay + " -----");
+            Action<Exception, TimeSpan> onBreak = (exception, timespan) =>
+            {
+                LogMessageToFile("On Break, time span : " + timespan.ToString());
             };
-            Action onReset = () => {
-                Console.WriteLine("On Reset");
+            Action onReset = () =>
+            {
+                LogMessageToFile("On Reset");
+            };
+            Action onHalfOpen = () =>
+            {
+                LogMessageToFile("on HalfOpen");
             };
             var circuitBreaker = Policy
                                 .Handle<Exception>()
                                 .CircuitBreakerAsync(
                                     exceptionsAllowedBeforeBreaking: 1,
                                     durationOfBreak: TimeSpan.FromMinutes(1),
-                                    onBreak : onBreak,
-                                    onReset : onReset
+                                    onBreak: onBreak,
+                                    onReset: onReset,
+                                    onHalfOpen: onHalfOpen
                                 );
 
             HttpResponseMessage res = null;
-            await circuitBreaker.ExecuteAsync(async() => res =  await ServiceCall(new Uri($"http://localhost:52892/api/products/GetdelayedProducts?delay={delay}")));
+            await circuitBreaker.ExecuteAsync(async () => res = await ServiceCall(new Uri($"http://localhost:52892/api/products/GetdelayedProducts?delay={delay}")));
             return await res.Content.ReadAsStringAsync();
         }
 
@@ -52,10 +92,14 @@ namespace Client_API.Controllers
             HttpResponseMessage response = null;
             try
             {
-                using (var client = new HttpClient() { Timeout = TimeSpan.FromSeconds(5) })
+                using (var client = new HttpClient()
+                {
+                    Timeout = TimeSpan.FromSeconds(5)
+                })
                 {
                     client.DefaultRequestHeaders.Accept.Clear();
                     client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    LogMessageToFile("Calling API");
                     response = await client.GetAsync(uri);
                     if (!response.IsSuccessStatusCode)
                     {
@@ -68,6 +112,22 @@ namespace Client_API.Controllers
                 throw ex;
             }
             return response;
+        }
+
+        public static void LogMessageToFile(string msg)
+        {
+            System.IO.StreamWriter sw = System.IO.File.AppendText(
+                 AppDomain.CurrentDomain.BaseDirectory + "My Log File.txt");
+            try
+            {
+                string logLine = System.String.Format(
+                    "{0:G}: {1}.", System.DateTime.Now, msg);
+                sw.WriteLine(logLine);
+            }
+            finally
+            {
+                sw.Close();
+            }
         }
 
 
--- Webapi_delay/Controllers/ProductsController.cs
+++ Webapi_delay/Controllers/ProductsController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using Webapi_delay.Models;
 
 namespace Webapi_delay.Controllers
@@ -11,11 +17,21 @@ namespace Webapi_delay.Controllers
     {
         private Product[] Products()
         {
-            return new Product[] { new Product() { ProductId = 1111, Title = "Fitness Nutrition Specialization (FNS)", Price = 499.00, ReleaseDate = DateTime.Now },
-                new Product() { ProductId = 2222, Title = "Weight Loss Specialization (WLS)", Price = 299.00, ReleaseDate = DateTime.Now },
-                new Product() { ProductId = 3333, Title = "Behavior Change Specialization (BCS)", Price = 699.00, ReleaseDate = DateTime.Now },
-                new Product() { ProductId = 4444, Title = "Group Personal Training Specialization (GPTS)", Price = 549.00, ReleaseDate = DateTime.Now }
-            };
+            
+
+            string text = File.ReadAllText(@"D:\Ascend\svn\POC\datafile.txt");
+            string[] ssss = text.Split(new string[] { "[data]" }, StringSplitOptions.None);
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Deserialize<Product[]>(ssss[1].Replace("\\", string.Empty));
+
+            //return new Product[]();
+
+            //return new Product[] { new Product() { ProductId = 1111, Title = "Fitness Nutrition Specialization (FNS)", Price = 499.00, ReleaseDate = DateTime.Now },
+            //    new Product() { ProductId = 2222, Title = "Weight Loss Specialization (WLS)", Price = 299.00, ReleaseDate = DateTime.Now },
+            //    new Product() { ProductId = 3333, Title = "Behavior Change Specialization (BCS)", Price = 699.00, ReleaseDate = DateTime.Now },
+            //    new Product() { ProductId = 4444, Title = "Group Personal Training Specialization (GPTS)", Price = 549.00, ReleaseDate = DateTime.Now }
+            //};
         }
 
         /// <summary>
@@ -26,6 +42,7 @@ namespace Webapi_delay.Controllers
         [HttpGet]
         public IEnumerable<Product> GetdelayedProducts(int delay = 5000)
         {
+
             System.Threading.Thread.Sleep(delay);
             return Products();
         }
